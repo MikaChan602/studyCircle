7a7cf5fea3045a0cce8aace86dbe38dae52ff6e5
Mika <amethyst602@gmail.com>
2023-11-26 22:57:36 +0800

    feat:三種實例

--- 2023-11-25/js/init.js
+++ 2023-11-25/js/init.js
@@ -28,7 +28,7 @@ class Normal extends List {
       // 第一個box
       const innerTextBox = document.createElement("div");
       innerTextBox.className = "col-1";
-      innerTextBox.appendChild(category);
+      innerTextBox.appendChild(category).append("基本");
       li.appendChild(innerTextBox);
       // 第二個box
       const textInput = document.createElement("div");
@@ -67,6 +67,8 @@ class Work extends List {
       li.className = "row";
       // 第一個box
       const innerTextBox = document.createElement("div");
+      const category = document.createElement("p");
+      innerTextBox.appendChild(category).append("工作");
       innerTextBox.className = "col-1";
       li.appendChild(innerTextBox);
       // 第二個box
@@ -92,6 +94,46 @@ class Work extends List {
   }
 }
 
+class Sport extends List {
+  createNew() {
+    const value = this.inputDom.value;
+    if (value === "") {
+      alert("輸入值不可為空");
+    } else {
+      // 操作DOM
+      let value = document.getElementById("inputData").value;
+      // to-do-list狀態
+      const li = document.createElement("li");
+      li.className = "row";
+      // 第一個box
+      const innerTextBox = document.createElement("div");
+      const category = document.createElement("p");
+      innerTextBox.appendChild(category).append("運動");
+      innerTextBox.className = "col-1";
+      li.appendChild(innerTextBox);
+      // 第二個box
+      const textInput = document.createElement("div");
+      textInput.className = "col-9";
+      const text = document.createTextNode(value);
+      textInput.appendChild(text);
+      // 第三個box
+      const btnDiv = document.createElement("div");
+      btnDiv.className = "col-2";
+      // 創建按鈕組件
+      const btn = document.createElement("button");
+      btn.className = "btn btn-warning";
+      btn.addEventListener("click", this.deleteBtn, false);
+      btn.appendChild(document.createTextNode("x"));
+      // 將按鈕組件加入div中
+      btnDiv.appendChild(btn);
+
+      li.appendChild(textInput);
+      li.appendChild(btnDiv);
+      this.bindDom.appendChild(li);
+    }
+  }
+}
+
 // 包裝
 function createList(listTarget, inputTarget) {
   const selectedItem = document.getElementById("selector").value;
@@ -100,6 +142,8 @@ function createList(listTarget, inputTarget) {
     return new Normal(listTarget, inputTarget).createNew();
   } else if (selectedItem === "work") {
     return new Work(listTarget, inputTarget).createNew();
+  } else if (selectedItem === "sport") {
+    return new Sport(listTarget, inputTarget).createNew();
   }
 }
 
