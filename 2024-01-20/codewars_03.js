87e943d8394fc1c03f42b2b172591793e7b05f47
GREATTREE\10167 <amethyst602@gmail.com>
2024-01-03 16:35:23 +0800

    doc: code wars 3

--- 2024-01-20/codewars_03.js
+++ 2024-01-20/codewars_03.js
@@ -1,18 +1,38 @@
 // Emma
 // [Which are in? from Emma](https://www.codewars.com/kata/550554fd08b86f84fe000a58)
 
-const a1 = ["arp", "live", "strong"]
+// const a1 = ["arp", "live", "strong"]
+
+// const a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
+
+const a1 = ["tarp", "mice", "bull"]
+
+const b1 = ["live", "strong", "arp"]
 
 const a2 = ["lively", "alive", "harp", "sharp", "armstrong"]
 
 // returns ["arp", "live", "strong"]
-
 function inArray(array1,array2){
-    let reg = '';
-
-    a1.forEach((value)=>{
-        reg = value;
-        
+    let arr = []
+    array1.forEach((value1)=>{
+        const regExp = new RegExp(value1)
+        // console.log('regExp',regExp);
+        array2.forEach((value2) =>{
+            // console.log('regExp Inner',regExp );
+            // console.log('a2,val',val);
+            // console.log('test', regExp.test(val));
+            if(regExp.test(value2)){
+                arr.push(value1)
+            }
+        })
     })
+    const newArr = [...new Set(arr)]
+    newArr.sort()
+    return newArr
+}
 
-  }
+
+const results = inArray(b1,a2)
+
+console.log('results', results);
+
