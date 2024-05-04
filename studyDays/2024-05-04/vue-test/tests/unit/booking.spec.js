4471f566b3277c0f3d267355a272b919ebff2c62
Mika <amethyst602@gmail.com>
2024-05-04 21:42:46 +0800

    feat: 修改讀書會日期＋PPT

--- studyDays/2024-05-04/vue-test/tests/unit/booking.spec.js
+++ studyDays/2024-05-04/vue-test/tests/unit/booking.spec.js
@@ -3,14 +3,11 @@ import booking from '@/views/reservation/booking'
 
 
 describe('booking.vue',()=>{
-    // TODO 初始值驗證
     /**  準備 arrange : 準備好受測目標需要的一切東西 */
-    let wrapper,vegetarian
+    let wrapper
 
     beforeEach(()=>{
-        wrapper = mount(booking)
-        vegetarian = wrapper.find('[data-test="vegetarian"]')
-        
+        wrapper = mount(booking) 
     })
     it('確認畫面初始狀況',()=>{
         /** 準備 Arrange */ 
@@ -24,37 +21,33 @@ describe('booking.vue',()=>{
             vegetarianNum: "",
         }
         // 2. 確認用元素
-        const vegetarianNum = wrapper.find('[data-test="vegetarianNum"]')
+        const vegetarianNum = wrapper.get('[data-test="vegetarianNum"]')
         /**  斷言 Assert */
         expect(wrapper.vm.params.data).toEqual(params) // 判斷初始參數
         expect(vegetarianNum.isVisible()).toBeFalsy() // 判斷該元素不顯示在畫面上但有渲染
-
-
     })
-    // TODO 畫面行為確認
+    //  畫面行為確認
     it('畫面行為確認,確認radio btn與吃素人數的顯示。',async()=>{
         /** 準備 Arrange */
-        const vegetarianNum = wrapper.find('[data-test="vegetarianNum"]')
-        const radio1 =  wrapper.find('[data-test="radio1"]') // 有吃素的radio btn
+        const vegetarianNum = wrapper.get('[data-test="vegetarianNum"]')
+        const radio1 =  wrapper.get('[data-test="radio1"]') // 有吃素的radio btn
         /** 操作 Act */ 
         await radio1.setValue('true') // 模擬點擊按鈕
         
         /** 斷言 Assert */ 
         expect(radio1.element.value).toBe('true')
-        expect(vegetarianNum.isVisible())
+        expect(vegetarianNum.isVisible()).toBe(true);
 
     })
-    // TODO 送出錯誤檢查
     
     it('確認使用者是否能正常輸入',async()=>{
         /* 準備 Arrange */
-        const nameInput = wrapper.find('[data-test="name"]')
-        const phoneInput = wrapper.find('[data-test="phone"]')
-        const storeSelect = wrapper.find('[data-test="selector"]')
-        const peopleInput = wrapper.find('[data-test="people"]')
-        const radio1 =  wrapper.find('[data-test="radio1"]') 
-        // TODO radio
-        const vegetarianNumInput = wrapper.find('[data-test="vegetarianNum"]')
+        const nameInput = wrapper.get('[data-test="name"]')
+        const phoneInput = wrapper.get('[data-test="phone"]')
+        const storeSelect = wrapper.get('[data-test="selector"]')
+        const peopleInput = wrapper.get('[data-test="people"]')
+        const radio1 =  wrapper.get('[data-test="radio1"]') 
+        const vegetarianNumInput = wrapper.get('[data-test="vegetarianNum"]')
 
         /* 操作 */
         await nameInput.setValue('Anna') // 賦予值
@@ -71,18 +64,15 @@ describe('booking.vue',()=>{
         expect(peopleInput.element.value).toBe('1')
         expect(radio1.element.value).toBe('true')
         expect(vegetarianNumInput.element.value).toBe('1')
-        
-        
-        // wrapper.find('[data-test=""]')
     })
     it('確認清除功能',async()=>{
         /* 準備 Arrange */
-        const nameInput = wrapper.find('[data-test="name"]')
-        const phoneInput = wrapper.find('[data-test="phone"]')
-        const storeSelect = wrapper.find('[data-test="selector"]')
-        const peopleInput = wrapper.find('[data-test="people"]')
-        const radio1 =  wrapper.find('[data-test="radio1"]') 
-        const clearBtn = wrapper.find('[data-test="clear"]')
+        const nameInput = wrapper.get('[data-test="name"]')
+        const phoneInput = wrapper.get('[data-test="phone"]')
+        const storeSelect = wrapper.get('[data-test="selector"]')
+        const peopleInput = wrapper.get('[data-test="people"]')
+        const radio1 =  wrapper.get('[data-test="radio1"]') 
+        const clearBtn = wrapper.get('[data-test="clear"]')
         const params = {
             name: "",
             phone: "",
@@ -91,8 +81,8 @@ describe('booking.vue',()=>{
             vegetarian: false,
             vegetarianNum: "",
         }
-        // TODO radio
-        const vegetarianNumInput = wrapper.find('[data-test="vegetarianNum"]')
+        // ! radio
+        const vegetarianNumInput = wrapper.get('[data-test="vegetarianNum"]')
 
         /* 操作 */
         await nameInput.setValue('Anna') // 賦予值
@@ -105,9 +95,6 @@ describe('booking.vue',()=>{
 
         /* 斷言 ..........................*/
         expect(wrapper.vm.params.data).toEqual(params) 
-
-        
-        // wrapper.find('[data-test=""]')
     })
 
     
